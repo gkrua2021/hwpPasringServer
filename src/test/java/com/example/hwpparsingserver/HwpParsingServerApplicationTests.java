5ae7daa363d28389a12ed5cef25dab7dfa36ee66
gkrua2021 <gkrua2021@naver.com>
2024-12-07 22:31:03 +0900

    start

---
+++ src/test/java/com/example/hwpparsingserver/HwpParsingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwpparsingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwpParsingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
