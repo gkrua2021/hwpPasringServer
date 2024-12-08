30e059beedc0adcfe4955193eb550c1c16628ad9
gkrua2021 <gkrua2021@naver.com>
2024-12-08 21:59:28 +0900

    2024-12-08 // 인증 커밋

---
+++ src/main/java/com/example/hwpparsingserver/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.hwpparsingserver.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable();
+
+        return http.build();
+    }
+}
